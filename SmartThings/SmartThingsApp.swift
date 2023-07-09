19a166d54f6038dcbfc4a0912eca5efb7274d804
Evdokimov Denis <analitik9@gmail.com>
2023-07-09 11:23:34 +0300

    Initial Commit

---
+++ SmartThings/SmartThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartThingsApp.swift
+//  SmartThings
+//
+//  Created by Denis Evdokimov on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
